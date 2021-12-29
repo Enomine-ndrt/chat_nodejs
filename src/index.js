f7defe87a06600bedd0c71196fa7112d487dc551
luis <luis.w0587@gmail.com>
2021-12-29 09:35:50 -0600

    binary

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-
-/**
- * ws
- *
- * @param {Function} connection - Establece conexion con websocket
+/**ws
+ * websocket
+ * @module websocket
+ * @requires express
+ * @requires http
+ * @requires ws
  */
+
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -11,20 +13,45 @@ const WebSocket = require("ws");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+/** 
+ * on connection 
+ * establece la conexion
+ * @name connection
+ * @function
+ * @param {Object} ws  recibe  conexion ws 
+ * */ 
 wss.on("connection", function connection(ws) {
+  /**
+   * on message
+   * @member {Object} ws
+   * @name message
+   * @function  
+   * @param {Object}  message recibe un  objeto mensaje
+   * @param {Boolean} isBinary recibe un tipo boleano
+   */
   ws.on("message", function incoming(message, isBinary) {
     console.log(message.toString(), isBinary);
-    /**
-     * Clients
-     * @param {Function} each - Trae los clientes
-     */
+  
+     /**
+      *clients each 
+      *@name clients
+      *@function 
+      *@param {WebSocket} client trae a los clientes
+       */
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
         /**
-         * send
-         * @param {string} - envia mensaje
-         * */
-        client.send(message.toString());
+         * send 
+         * @name send
+         * @function
+         * @param {string} message  mensaje obtenido
+         */
+        if(!binary){
+          client.send(message.toString());
+        }else{
+          client.send("binario");
+        }
+        
       }
     });
   });
@@ -32,6 +59,8 @@ wss.on("connection", function connection(ws) {
 
 /**
  * Get
+ * @name GET
+ * @function
  * @param {String} "/" - Punto de entrada
  * @param {Object} req - Request object
  * @param {Object} res - Response object
@@ -42,8 +71,10 @@ app.get("/", (req, res) => {
 
 /**
  * Sever listen
- * 
- * @param {number} process.env.PORT - puerto usado
+ * @name listen
+ * @function 
+ * @param {number} process.env.PORT - puerto usado por variable de ambiente
+ * @param {number} '5000' puerto por default
  */
 server.listen(process.env.PORT || 5000, "0.0.0.0", () => {
   console.log(`Listen on port ${process.env.PORT || 5000}`);
